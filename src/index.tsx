91a35e933258cdcdb3cfaf363d77aa79b0426f3c
Ivan Kondratyev <ivanbright@gmail.com>
2024-06-29 16:48:28 +0200

    Plugging eslint-import plugin.

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,6 @@ import { greet } from '@root/lib';
 import './index.scss';
 
 const App = () => <h1 className="app">{greet('World')}</h1>;
-
 const mountPoint = document.getElementById('react-root');
 const root = createRoot(mountPoint);
-
 root.render(<App />);
